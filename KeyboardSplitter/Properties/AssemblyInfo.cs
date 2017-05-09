5c9aa2a3a7ed66e6725e463b6dff9493fd9d3b76
djlastnight <djlastnight@gmail.com>
2017-05-09 13:20:14 +0300

    Updated to version 1.2.0.2

--- KeyboardSplitter/Properties/AssemblyInfo.cs
+++ KeyboardSplitter/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("djlastnight")]
 [assembly: AssemblyProduct("KeyboardSplitter")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.2.0.2")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
